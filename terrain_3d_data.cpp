c9199d8a69d593aeedd1f31f2e3be74fb2dfc533
DanielSnd <daniel@bittentoast.com>
2025-07-06 15:16:47 -0300

    Fixed include for 4.5

--- terrain_3d_data.cpp
+++ terrain_3d_data.cpp
@@ -5,7 +5,7 @@
 #include "terrain_3d.h"
 
 #ifdef TOOLS_ENABLED
-#include "editor/editor_file_system.h"
+#include "editor/file_system/editor_file_system.h"
 #include "editor/editor_interface.h"
 #endif
 #include "terrain_3d_texture_asset.h"
